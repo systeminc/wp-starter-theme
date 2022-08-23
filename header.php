f8dc1a875009a85d83c400af968aa051bf20ee01
Mladen Janjetovic <mladen@janjetovic.com>
2022-08-23 12:55:42 +0200

    favicon fix

--- header.php
+++ header.php
@@ -7,7 +7,6 @@
 	<title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
 
     <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
     <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
