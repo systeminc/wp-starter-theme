ae5a442b3a93425b51a4cac4ede3de1751d53a39
Mladen Janjetovic <mladen@janjetovic.com>
2023-03-23 18:47:04 +0100

    global JS functions

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,16 @@
+/* Global functions */
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+function empty(v){
+    return !v || typeof v == 'undefined' || ( typeof v == 'string' && v.replace(/[0| ]+/gm,'')==='' )	|| ( typeof v == 'object' && !Object.size(v) );
+}
+
 (function ($) {
     $(document).ready(function () {
         
